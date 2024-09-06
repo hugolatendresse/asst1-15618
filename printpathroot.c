6eb86614fb749f73614e194017f30a47c5333bd3
Hugo Latendresse <hugolatendresse@gmail.com>
2024-09-06 12:26:29 -0400

    git .idea/ folder

---
+++ printpathroot.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+
+int main() {
+    char cwd[PATH_MAX];
+
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        printf("in my root, found the working directory! %s\n", cwd);
+    } else {
+        perror("getcwd() error");
+    }
+
+    return 0;
+}
